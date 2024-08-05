b0299cfd8823b06adb7a9ed446348cec0b52c823
Leticia <leticia-ribeiro98@hotmail.com>
2024-08-05 00:26:19 -0300

    Realizei o pd merge, aceitando as alterações realizadas anteriormente.

--- jogo/src/jogo_principal.cpp
+++ jogo/src/jogo_principal.cpp
@@ -45,20 +45,6 @@ void desenharMenu(sf::RenderWindow& window, std::string& jogador1, std::string& 
 
 int main()
 {
-<<<<<<< HEAD
-    // Cria a janela com resolução de 800x600
-    float res_x = 800.0f;
-    float res_y = 600.0f;
-    sf::RenderWindow window(sf::VideoMode(res_x, res_y), "Deluxe Edition");
-
-    // Define uma variável de cor com valores RGB (vai de 0 a 255)
-    sf::Color cor(255, 130, 190);
-
-    // Cria um círculo com raio 50
-    sf::CircleShape circulo(50.f);
-
-    // Define a cor de preenchimento do círculo para azul
-=======
     //--------------- MÓDULO PARA INICIAR A TELA DO JOGO NAS DIMENSÕES DA TELA DA PESSOA-----------------//
     // Obtém as dimensões da tela
     RECT screenRect;
@@ -82,7 +68,6 @@ int main()
     // Configurações iniciais de cor e forma
     sf::Color cor(255, 130, 190); //variavel com parametros de valor de cor (vai de 0 a 255)
     sf::CircleShape circulo(50.f);
->>>>>>> origin/campos
     circulo.setFillColor(sf::Color::Blue);
 
     // Define a posição inicial do círculo
@@ -91,35 +76,17 @@ int main()
     // Cria dois pontos com coordenadas diferentes
     PontoF pos1(2.0,3.0);
     PontoF pos2(200.0,200.0);
-<<<<<<< HEAD
-
-    // Cria um objeto Botao e configura suas propriedades
-    Botao loginBox;
-    loginBox.criarCampo(300, 50, "arial.ttf");
-=======
->>>>>>> origin/campos
     
     // Cria um relógio para medir o tempo
     sf::Clock relogio;
-<<<<<<< HEAD
-
-    // Soma as coordenadas de dois pontos e armazena em um terceiro ponto
-    PontoF pos3 = pos1 + pos2;
-
-    // Cria outro botão com posição, tamanho e cor definidos
-    Botao botao1(pos1, 5*10.0, 5*10.0, sf::Color::Red);
-=======
     
     PontoF pos3 = pos1 + pos2;
 
     //cout << pos3 << endl;
->>>>>>> origin/campos
 
     // Cria um objeto Movimentacao para gerenciar o movimento
     Movimentacao movimentacao;
 
-<<<<<<< HEAD
-=======
     // Flag para controle dos menus
     bool menuInicial = true;
     // Flag para alternar entre os jogadores
@@ -127,7 +94,6 @@ int main()
     std::string jogador1;
     std::string jogador2;
 
->>>>>>> origin/campos
     // Roda o programa enquanto a janela estiver aberta
     while (window.isOpen())
     {
@@ -136,55 +102,6 @@ int main()
         
         while (window.pollEvent(event)) //pollEvent é uma função que retorna true se houver eventos na fila
         {
-<<<<<<< HEAD
-            // Se o evento for de fechamento da janela, fecha a janela
-            if (event.type == sf::Event::Closed)
-                window.close();
-            
-            // Verifica se o botão do mouse foi pressionado
-            if (event.type == sf::Event::MouseButtonPressed) {
-
-                // Simples verificação de clique no botão
-                if (loginBox.getForma().getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-                    loginBox.setFocado(true); // Foca no loginBox se clicado
-                } else {
-                    loginBox.setFocado(false); // Desfoca se clicado fora
-                }
-            }
-
-            // Se loginBox está focado, recebe entrada de texto
-            if (loginBox.isFocado) {
-                loginBox.receberInput(event);
-            }
-
-            // Verifica se uma tecla foi pressionada
-            if (event.type == sf::Event::KeyPressed) {
-                movimentacao.mover(circulo, event.key.code);
-            }
-            if (event.type == sf::Event::KeyPressed) {
-                movimentacao.mover(botao1.getForma(), event.key.code);
-            }
-            //Verifica se 0.5 segundo se passou
-            if (relogio.getElapsedTime().asSeconds() >= 0.5f) {
-                cout << "Centro: " << botao1.getCentro() << endl;
-                cout << "Posicao: " << botao1.getPosicao()<< endl;
-                cout << "Tamanho: " << botao1.tamX << " " << botao1.tamY << endl;
-                relogio.restart(); // Reinicia o clock
-            }
-
-        }
-        
-        // Limpa a janela com a cor definida
-        window.clear(cor);
-        // Desenha os elementos na janela
-        window.draw(loginBox.getForma());
-        window.draw(loginBox.getTexto());
-        window.draw(botao1.getForma());
-        window.draw(circulo);
-
-        // Atualiza a tela com o que foi desenhado
-        window.display();
-=======
             if (event.type == sf::Event::Closed){
                 window.close();
             }
@@ -203,7 +120,6 @@ int main()
             //window.draw(botao1.getForma());
             //window.draw(circulo);
         }
->>>>>>> origin/campos
     }
     return 0;
 }

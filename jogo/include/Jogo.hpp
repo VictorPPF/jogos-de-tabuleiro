c9a48be194186a87f6bca54f292062420c36d1af
Victor <ctt.victorpinto@gmail.com>
2024-08-15 02:23:01 -0300

    Atributo jogo_atual adicionado

--- jogo/include/Jogo.hpp
+++ jogo/include/Jogo.hpp
@@ -11,11 +11,12 @@ protected:
     Jogador* player2; // Ponteiro para o jogador2
     int status;       // Status do jogo
     std::string jogador_atual;
+    std::string jogo_atual;
 
 public:
     // Construtor
-    Jogo(Jogador* jogador1, Jogador* jogador2) : player1(jogador1), player2(jogador2), status(0), jogador_atual(player1->getApelido()) {}
-    Jogo():player1(nullptr), player2(nullptr),status(-1), jogador_atual("-1"){};
+    Jogo(Jogador* jogador1, Jogador* jogador2, std::string jogo_escolhido) : player1(jogador1), player2(jogador2), status(0), jogador_atual(player1->getApelido()), jogo_atual(jogo_escolhido) {}
+    Jogo():player1(nullptr), player2(nullptr),status(-1), jogador_atual("-1"), jogo_atual("-1"){};
 
     // Destrutor virtual
     virtual ~Jogo() {}

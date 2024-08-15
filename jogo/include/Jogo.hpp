63bd888e093a422fc6b8b58c7782a4f2d1b6d05b
Victor <ctt.victorpinto@gmail.com>
2024-08-15 02:09:02 -0300

    Classe abstrata Jogo feita

---
+++ jogo/include/Jogo.hpp
@@ -0,0 +1,35 @@
+#ifndef JOGO_HPP
+#define JOGO_HPP
+
+#include <string>
+#include "Jogador.hpp"
+
+// Classe abstrata Jogo
+class Jogo {
+protected:
+    Jogador* player1; // Ponteiro para o jogador1
+    Jogador* player2; // Ponteiro para o jogador2
+    int status;       // Status do jogo
+    std::string jogador_atual;
+
+public:
+    // Construtor
+    Jogo(Jogador* jogador1, Jogador* jogador2) : player1(jogador1), player2(jogador2), status(0), jogador_atual(player1->getApelido()) {}
+    Jogo():player1(nullptr), player2(nullptr),status(-1), jogador_atual("-1"){};
+
+    // Destrutor virtual
+    virtual ~Jogo() {}
+
+    // Métodos abstratos que devem ser implementados pelas classes derivadas
+    virtual bool condicao_vitoria() const = 0; // Verifica a condição de vitória
+    virtual bool jogada_valida() const = 0;    // Verifica se a jogada é válida
+
+    // Métodos acessores
+    std::string get_jogador_atual() const { return jogador_atual; }
+    bool get_status() const { return status; }
+
+    // Método para alterar o status
+    void set_status(bool novo_status) { status = novo_status; }
+};
+
+#endif

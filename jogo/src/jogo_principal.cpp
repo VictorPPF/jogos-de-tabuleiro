2cb16c0ddc16eee934edc4513aa377a26f755e18
Victor <ctt.victorpinto@gmail.com>
2024-08-16 00:04:56 -0300

    Implementando logica para login dos jogadores

--- jogo/src/jogo_principal.cpp
+++ jogo/src/jogo_principal.cpp
@@ -168,11 +168,25 @@ int main() {
                 telaMenu.campoJogador1.processarEventos(event, window);
                 telaMenu.campoJogador2.processarEventos(event, window);
                 
+                if(telaMenu.campoJogador1.deu_enter==1){
+                    Jogador* jogador1 = new Jogador(telaMenu.campoJogador1.obterTexto());
+                    if(jogador1->existeConta(telaMenu.campoJogador1.obterTexto())){
+                        std::cout<< "Jogador 1 foi logado com sucesso!" <<std::endl;
+                    }
+                }
+                if(telaMenu.campoJogador2.deu_enter==1){
+                    Jogador* jogador2 = new Jogador(telaMenu.campoJogador2.obterTexto());
+                    if(jogador2->existeConta(telaMenu.campoJogador2.obterTexto())){
+                        std::cout<< "Jogador 2 foi logado com sucesso!" <<std::endl;
+                    }
+                    
+                }
 
             }
             if (estadoAtual == "Cadastro") {
                 telaCadastro.campoNome.processarEventos(event, window);
                 telaCadastro.campoApelido.processarEventos(event, window);
+
                 if(telaCadastro.campoNome.deu_enter==1 && telaCadastro.campoApelido.deu_enter==1){
                     std::cout<< "deu enter nos dois\n";
                     telaCadastro.campoNome.deu_enter=0;

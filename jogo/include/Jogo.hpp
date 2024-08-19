f88e01d733a159d6182ad0a4cd6e5776f7828a6a
Leticia <leticia-ribeiro98@hotmail.com>
2024-08-18 22:42:57 -0300

    alterei a ordem

--- jogo/include/Jogo.hpp
+++ jogo/include/Jogo.hpp
@@ -9,11 +9,10 @@ class Jogo {
 public:
 
     // Métodos abstratos que devem ser implementados pelas classes derivadas
+    virtual void desenharJogo() = 0;
     virtual void acao() = 0;
     virtual void LimpaTabuleiro() = 0;
-    virtual void desenharJogo() = 0;
     virtual void setJogadores (Jogador& player1, Jogador& player2) = 0;
-    virtual bool verificaCondicaoVitoria() = 0;
     //virtual bool jogada_valida(int x, int y, int jogador) = 0;    // Verifica se a jogada é válida
 
     // Métodos acessores
